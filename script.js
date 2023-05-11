d67f743195ccf5686ccaf843ce4ef2d7b33eca9d
liviza <leviza00@mail.ru>
2023-05-11 18:20:39 +0300

    init: start game project

---
+++ script.js
@@ -0,0 +1,93 @@
+"use strict";
+
+const player0El = document.querySelector(".player--0");
+const player1El = document.querySelector(".player--1");
+const score0El = document.querySelector("#score--0");
+const score1El = document.getElementById("score--1");
+const current0El = document.getElementById("current--0");
+const current1El = document.getElementById("current--1");
+const diceEl = document.querySelector(".dice");
+const btnNew = document.querySelector(".btn--new");
+const btnRoll = document.querySelector(".btn--roll");
+const btnHold = document.querySelector(".btn--hold");
+
+let scores, currentScore, activePlayer, playing;
+
+// Начальные условия
+const init = function () {
+  scores = [0, 0];
+  currentScore = 0;
+  activePlayer = 0;
+  playing = true;
+
+  score0El.textContent = 0;
+  score1El.textContent = 0;
+  current0El.textContent = 0;
+  current1El.textContent = 0;
+
+  diceEl.classList.add("hidden");
+  player0El.classList.remove("player--winner");
+  player1El.classList.remove("player--winner");
+  player0El.classList.add("player--active");
+  player1El.classList.remove("player--active");
+};
+init();
+
+const switchPlayer = function () {
+  document.getElementById(`current--${activePlayer}`).textContent = 0;
+  currentScore = 0;
+  activePlayer = activePlayer === 0 ? 1 : 0;
+  player0El.classList.toggle("player--active");
+  player1El.classList.toggle("player--active");
+};
+
+// Функционал игры
+btnRoll.addEventListener("click", function () {
+  if (playing) {
+    // 1. Создание случайного числа
+    const dice = Math.trunc(Math.random() * 6) + 1;
+
+    // 2. Отображение кости
+    diceEl.classList.remove("hidden");
+    diceEl.src = `img/dice-${dice}.png`;
+    console.log(dice);
+
+    // 3. Проверка на 1: если да, сменить игрока
+    if (dice !== 1) {
+      // Добавление кости к текущему счету
+      currentScore += dice;
+      document.getElementById(`current--${activePlayer}`).textContent =
+        currentScore;
+    } else {
+      // Смена игрока
+      switchPlayer();
+    }
+  }
+});
+
+btnHold.addEventListener("click", function () {
+  if (playing) {
+    // 1. Добавление текущего счета к общему счету активного игрока
+    scores[activePlayer] += currentScore;
+    document.getElementById(`score--${activePlayer}`).textContent =
+      scores[activePlayer];
+    // 2. Проверка общего счета активного игрока на 100
+    if (scores[activePlayer] >= 100) {
+      // Конец игры
+      playing = false;
+      diceEl.classList.add("hidden");
+
+      document
+        .querySelector(`.player--${activePlayer}`)
+        .classList.add("player--winner");
+      document
+        .querySelector(`.player--${activePlayer}`)
+        .classList.remove("player--active");
+    } else {
+      // Смена игрока
+      switchPlayer();
+    }
+  }
+});
+
+btnNew.addEventListener("click", init);
